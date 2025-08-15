8f5908c10746d2370940eb24cbd543d6fb70adbf
Makoto Emura <makoto@emurasoft.com>
2025-08-14 17:27:32 -0700

    Output country

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,21 @@ async fn fetch(
 ) -> Result<Response> {
     console_error_panic_hook::set_once();
 
+    // Get the headers from the incoming request
+    let headers = req.headers();
+
+    // Get the value of the "CF-IPCountry" header.
+    // This header is automatically added by Cloudflare.
+    // We use unwrap_or_else to provide a default value if it's somehow missing.
+    let country = headers.get("CF-IPCountry")?
+        .unwrap_or_else(|| "unknown".to_string());
+
     console_log!(
-        "Received request: method={}, url={}",
+        "Received request: method={}, url={}, country={}",
         req.method().to_string(),
-        req.url()?.to_string()
+        req.url()?.to_string(),
+        country
     );
-    Response::ok("Hello world!")
+
+    Response::ok(country)
 }
