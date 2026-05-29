ff74eb4c57c982f059280945e4f1034b246de306
Makoto <36318069+MakotoE@users.noreply.github.com>
2026-05-28 17:50:28 -0700

    Fix ALLOWED_ORIGINS (#21)

--- src/lib.rs
+++ src/lib.rs
@@ -145,11 +145,11 @@ const ALLOWED_ORIGINS: phf::Set<&'static str> = phf_set! {
     "https://zh-tw.emeditor.com",
     "https://ru.emeditor.com",
     "https://emeditor.wpengine.com",
-    "https://emeditorde.wpengine.com/",
-    "https://emeditorjp.wpengine.com/",
-    "https://emeditorko.wpengine.com/",
-    "https://emeditorzhcn.wpengine.com/",
-    "https://emeditorzhtw.wpengine.com/",
+    "https://emeditorde.wpengine.com",
+    "https://emeditorjp.wpengine.com",
+    "https://emeditorko.wpengine.com",
+    "https://emeditorzhcn.wpengine.com",
+    "https://emeditorzhtw.wpengine.com",
 };
 
 fn normalize_origin(origin: &str) -> &str {
